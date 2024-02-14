ae874881fcf2860f85b38098c7c15c69c24140a9
CodingByRajeev <159898203+Cod-Rajeev@users.noreply.github.com>
2024-02-14 17:25:55 +0000

    Pending changes exported from your codespace

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Welome to GitHub programing</h1>
+        <img src='https://images.pexels.com/photos/13306877/pexels-photo-13306877.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' width="50%"/>
+        <img src='https://images.pexels.com/photos/16658578/pexels-photo-16658578/free-photo-of-eagle.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' width="50%" height="369"/>
+
+    </div>
+  )
+}
+
+export default Home
